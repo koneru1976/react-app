a921e119b57cc33e263682cad41f555f4e40bceb
Ramakrishna Koneru <Ramakrishna.Koneru@gmail.com>
2019-05-25 20:00:46 -0500

    Sirisha - Built Hello Component

---
+++ src/hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './hello.css';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Hello..Fist React app. 
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
